3e11582a2862a65c7c0b7e49bcb60c7aa1a29cdd
elijah7000 <143031360+Elijah-J-Kim@users.noreply.github.com>
2024-04-20 16:22:07 -0400

    Initial commit

---
+++ src/main/java/edu/iu/c322/test3/service/TokenService.java
@@ -0,0 +1,37 @@
+package edu.iu.c322.test3.service;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+@Service
+public class TokenService {
+    private final JwtEncoder encoder;
+
+    public TokenService(JwtEncoder encoder) {
+        this.encoder = encoder;
+    }
+
+    public String generateToken(Authentication authentication) {
+        Instant now = Instant.now();
+        String scope = authentication
+                .getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+        Instant expirationTime = now.plusSeconds(1 * 60 * 60); // Add 1 hour in seconds
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(expirationTime)
+                .subject(authentication.getName())
+                .claim("scope", scope)
+                .build();
+        return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+}
